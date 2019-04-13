b524d5f57314c8570304b3b3fb7952a854a2fe3a
Kyle S Gloria <kgcodemail@gmail.com>
2019-04-13 00:29:30 -0700

    Added scripts to control stats: work in progress

---
+++ Assets/Scripts/StatsController.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StatsController : MonoBehaviour
+{
+
+    public int SkillPts;
+    public int ExpPoints;
+
+    private int Atk;
+    private int Def;
+    private int Str;
+    private int Special;
+
+    private int HitPoints = 100;
+    private int BossHP;
+    // Start is called before the first frame update
+    void newGame()
+    {
+        SkillPts = 0;
+        ExpPoints = 0;
+        Atk = 0;
+        Def = 0;
+        Str = 0;
+        Special = 0;
+
+    }
+
+    void setHP(int num)
+    {
+        BossHP = num;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(HitPoints == 0)
+        {
+            //insert game over screen
+        }
+
+        if(BossHP == 0)
+        {
+            //fetch next scene
+        }
+
+    }
+
+
+
+    public void SkillPtsup(bool leveld)
+    {
+        if (leveld == true) SkillPts += 2;
+    }
+
+    public void incStat(int Stattype)
+    {
+        if (Stattype == 1) Atk += 1;
+        else if (Stattype == 2) Def += 1;
+        else if (Stattype == 3) Str += 1;
+        else if (Stattype == 4) Special += 1;
+    }
+
+    public void calcDMG()
+    {
+
+    }
+}
